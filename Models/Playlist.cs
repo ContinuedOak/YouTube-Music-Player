3942587ef6bfde097bb614253b1cb88d3f3c3dc6
ContinuedOak <118535044+ContinuedOak@users.noreply.github.com>
2026-09-23 05:15:04 +1000

    Release

---
+++ Models/Playlist.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace OakMusic.Models
+{
+    public class Playlist
+    {
+        public string Id { get; set; } = "";
+
+        public string Name { get; set; } = "";
+
+        public string IconPath { get; set; } = "";
+
+        public List<Song> Songs { get; set; } = new();
+    }
+}
---
+++ Models/song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OakMusic.Models
+{
+    public class Song
+    {
+        public string VideoId { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Artist { get; set; } = "";
+        public string ChannelName { get; set; } = "";
+        public string Album { get; set; } = "";
+        public string ThumbnailUrl { get; set; } = "";
+        public TimeSpan Duration { get; set; }
+
+        public string DurationText =>
+            Duration.TotalHours >= 1
+                ? Duration.ToString(@"h\:mm\:ss")
+                : Duration.ToString(@"m\:ss");
+    }
+}
